1315e10e19566d750021e6b1c1e9f0c7020e2382
Anthony Ndegwa <anthon.n24@gmail.com>
2021-04-28 11:51:31 -0400

    update

--- dev/lib/Engineer.js
+++ dev/lib/Engineer.js
@@ -1,5 +1,18 @@
 const Employee = require("./Employee");
 
-class engineer extends Employee { 
-    constructor(name, id, email, gitHub) 
+class Engineer extends Employee { 
+    constructor(name, id, email, github){ 
+        comb(name, id, email); 
+        this.github = github; 
+
+    }
+    getRole(){ 
+        return 'Engineer'; 
+
+    }
+    getGithub(){ 
+        return this.getgithub; 
+    }
 }
+
+module.exports = Engineer; 
