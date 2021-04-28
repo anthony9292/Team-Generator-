3f8a07099fc0d2fa4143d861add22d1d77b79d1e
Anthony Ndegwa <anthon.n24@gmail.com>
2021-04-28 12:01:14 -0400

    update

--- dev/lib/Intern.js
+++ dev/lib/Intern.js
@@ -3,18 +3,13 @@ const Employee = require("./Employee");
 class Intern extends Employee { 
     constructor(name, id, email, University){ 
          super(name, id, email); 
-         this.University = University; 
-
+        this.University = University;
 }
- 
 getRole() { 
     return 'Intern'; 
 }
-
 getUniversity() { 
     return this.University; 
 }
-
 } 
-
 module.exports = Intern
