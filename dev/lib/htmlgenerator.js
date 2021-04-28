b9b8e0b980722fa8855bc887343576867d26dab6
Anthony Ndegwa <anthon.n24@gmail.com>
2021-04-28 12:18:36 -0400

    update

--- dev/lib/htmlgenerator.js
+++ dev/lib/htmlgenerator.js
@@ -6,15 +6,15 @@ const htmltempsDir = path.resolve(__dirname, "../htmltemps");
 const render = employees => { 
     const html = [];  
 
-    html.push(employees.filter(employees => employees.getrole() === "Manager")
+    html.push(employees.filter(employees => employees.getRole() === "Manager")
     .map(manager =>renderManager(manager))
     ); 
     
-    html.push(employees.filter(employees => employees.getrole() === "Manager")
-    .map(manager =>renderManager(manager))
+    html.push(employees.filter(employees => employees.getRole() === "Engineer")
+    .map(engineer=>renderEngineer(engineer))
     ); 
     
-    html.push(employees.filter(employees => employees.getrole() === "Manager")
-    .map(manager =>renderManager(manager))
+    html.push(employees.filter(employees => employees.getRole() === "Intern")
+    .map(intern =>renderIntern(intern))
     ); 
 }
