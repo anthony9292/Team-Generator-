667a23ad4c20ee9fafc1f9c423b6991ad7a71760
Anthony Ndegwa <anthon.n24@gmail.com>
2021-04-28 10:06:11 -0400

    update

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const Manager = require("./dev/lib/Manager"); 
 const Engineer = require("./dev/lib/Engineer"); 
 const Intern = require("./div/lib/Intern");
+const inquirer = require("inquirer"); 
+const path = require("path"); 
+const fs = require("fs"); 
+const { request } = require("node:http");
 
+const OutPut_Dir = path.resolve(__dirname, "outputs"); 
+const outputPath = path.join(OutPut_Dir, "team.html"); 
+
+const render = require("./dev/lib/htmlgenerator"); 
+
+const teamArray = []; 
+
+const 
