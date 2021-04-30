348e92391274f238aab706c7a35616dfaacc519e
Anthony Ndegwa <anthon.n24@gmail.com>
2021-04-30 18:25:58 -0400

    update

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 const inquirer = require("inquirer")
 const fs = require("fs");
-const
+const style = require("./dev/templets/css") 
+
+const Employee = require("./dev/lib/employee")
+const Intern = require("./dev/lib/intern")
+const Manager = require("./dev/lib/manager")
+const Engineer = require("./dev/lib/engineer")
+
+let finalTeamArray = []; 
+
+function startingPrompt() { 
+    inquirer.prompt([
+            {
+
+         
+        messege:"/////////Welcome to team Generator Web Page! To get started please write our team name://///////", 
+    }
+
+    ])
+    .then(function(data){ 
+        const teamName = data.teamname
+        finalTeamArray.push(teamName)
+        addManager(); 
+    })
+
+}
+
+function addManager() |{ 
+    inquirer.prompt([
+          
+        
+    ])
+}
--- dev/templets/css.js
+++ dev/templets/css.js
@@ -1,1 +1,16 @@
+const style = `
 
+html, body { 
+   
+    max-width: 100% !important; 
+    overflow-x: hidden !important;
+
+}
+
+body { 
+      display: flex; 
+      flex-direction: column;
+      align-items: center; 
+}
+
+` 
